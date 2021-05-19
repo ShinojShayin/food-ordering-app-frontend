fab9bbf5b0e71158583034557464a66df862a342
shaziap <shaiz.sit@gmail.com>
2021-05-20 00:10:25 +0530

    Added Stepper and Tabs

--- src/common/Controller.js
+++ src/common/Controller.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "../screens/home/Home";
+import Checkout from "../screens/checkout/Checkout";
 
 class Controller extends Component {
   render() {
     return (
       <Router>
-        <Route exact path="/" render={(props) => <Home {...props} />} />
+        <Route exact path="/" render={(props) => <Checkout {...props} />} />
       </Router>
     );
   }
